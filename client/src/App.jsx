ebaf9dce55b5e67a702d4d302a02cee7dc656913
danieltrusca <truscadani@yahoo.com>
2024-09-30 16:11:26 +0300

    install react, tailwind and create first template

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="font-bold text-3xl">App</div>
+    </div>
+  );
+};
+
+export default App;
