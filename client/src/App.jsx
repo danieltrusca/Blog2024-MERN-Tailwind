9b54eb5a4ac8befe31d40aedb4d777124be1d49c
danieltrusca <truscadani@yahoo.com>
2024-10-01 15:15:22 +0300

    install react-router-dom and create few pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
+import { Routes, Route } from "react-router-dom";
+
+// pages
+import { About, Dashboard, Home, Projects, Signin, Signup } from "./pages";
+
 const App = () => {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <div className="font-bold text-3xl">App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 };
